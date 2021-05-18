f0fc4a4979e80688c1814986f3fb8b681cd05487
crake7 <victorm_5_8@hotmail.com>
2021-05-17 21:01:50 -0400

    New

---
+++ Priv-esc/process_monitor.py
@@ -0,0 +1,36 @@
+import wmi
+
+
+def log_to_file(message):
+    with open('process_monitor_log.csv', 'a') as fd:
+        fd.write(f'{message}\r\n')
+
+def monitor():
+    head = 'CommandLine, Time, Executable, Parent PID, PID, User, SID, Privileges'
+    log_to_file(head)
+    c = wmi.WMI()
+    process_watcher = c.Win32_Process.watch_for('creation')   # Returns a 'New Process' event
+    while True:
+        try:
+            new_process = process_watcher()         # The event is a Win32_Process WMI class
+            cmdline = new_process.CommandLine
+            create_date = new_process.CreationDate
+            executable = new_process.ExecutablePath
+            parent_pid = new_process.ParentProcessId
+            pid = new_process.ProcessId
+            proc_owner = new_process.GetOwner()
+            owner_sid = new_process.GetOwnerSid()
+
+            privileges  = 'N/A'
+            process_log_message = (
+                f'{cmdline} , {create_date} , {executable} ,'
+                f'{parent_pid} , {pid} , {proc_owner} , {owner_sid} , {privileges}'
+                )
+            print(process_log_message)
+            print()
+            log_to_file(process_log_message)
+        except Exception:
+            pass
+        
+if __name__ == '__main__':
+    monitor()
